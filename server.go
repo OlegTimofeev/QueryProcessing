0db6b7eb87384422a177329224620f47d572b19a
OlegTimofeev <subzerotim@gmail.com>
2019-02-27 23:57:50 +0200

    fix

--- server.go
+++ server.go
@@ -9,10 +9,10 @@ import (
 )
 
 type FetchTask struct {
-	REQUEST string `json:"request"`
-	METHOD  string `json:"method"`
-	HEADERS string `json:"headers"`
-	BODY    string `json:"body"`
+	Request string `json:"request"`
+	Method  string `json:"method"`
+	Headers string `json:"headers"`
+	Body    string `json:"body"`
 }
 
 var count int = 1
@@ -20,22 +20,25 @@ var fetchTasks = make(map[string]FetchTask)
 
 func main() {
 	r := echo.New()
-	fetchTasks[strconv.Itoa(count)] = FetchTask{REQUEST: "test1", METHOD: "GET", HEADERS: "HEAD", BODY: "BODY"}
+	fetchTasks[strconv.Itoa(count)] = FetchTask{Request: "test1", Method: "GET", Headers: "HEAD", Body: "BODY"}
 	count++
-	fetchTasks[strconv.Itoa(count)] = FetchTask{REQUEST: "test2", METHOD: "GET", HEADERS: "HEAD", BODY: "BODY"}
+	fetchTasks[strconv.Itoa(count)] = FetchTask{Request: "test2", Method: "GET", Headers: "HEAD", Body: "BODY"}
 	count++
 	fmt.Println("Server is listening...")
-	r.GET("/getTasks", getTasks)
-	r.GET("/getTask/:id", getTask)
-	r.POST("/addTask", addFetchTask)
-	r.DELETE("/deleteTask/:id", deleteFetchTask)
+	r.GET("/task", getTasks)
+	r.GET("/task/:id", getTask)
+	r.POST("/task", addFetchTask)
+	r.DELETE("/task/:id", deleteFetchTask)
 	log.Fatal(http.ListenAndServe(":8000", r))
 
 }
 
 func addFetchTask(c echo.Context) error {
 	var ft FetchTask
-	c.Bind(&ft)
+	var err error
+	if err = c.Bind(&ft); err != nil {
+		return c.String(http.StatusBadRequest, "Wrong JSON")
+	}
 	fetchTasks[strconv.Itoa(count)] = ft
 	count++
 	return c.String(http.StatusOK, "Task added")
@@ -61,5 +64,5 @@ func deleteFetchTask(c echo.Context) error {
 		delete(fetchTasks, taskId)
 		return c.String(http.StatusOK, "Task deleted")
 	}
-	return c.String(http.StatusOK, "Task not found")
+	return c.String(http.StatusNotFound, "Task not found")
 }
