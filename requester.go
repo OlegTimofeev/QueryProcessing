8be2fe45f41635ead83dcc72e8c65b0bbc129ffe
OlegTimofeev <subzerotim@gmail.com>
2019-03-15 15:38:01 +0200

    Инкапсуляция2

--- requester.go
+++ requester.go
@@ -4,4 +4,5 @@ import "net/http"
 
 type Requester interface {
 	send(task *FetchTask) (http.Response, error)
+	saveResponse(task *FetchTask, resp *http.Response) error
 }
--- server.go
+++ server.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"github.com/labstack/echo"
 	"github.com/rs/xid"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -23,24 +22,44 @@ type FetchTask struct {
 type MapStore struct {
 	fetchTasks map[string]FetchTask
 }
-type RequesterTool struct{}
+type RequesterTool struct {
+	requests map[string]http.Response
+}
+
+func (ms *MapStore) init() {
+	ms.fetchTasks = make(map[string]FetchTask)
+}
+func (rt *RequesterTool) init() {
+	rt.requests = make(map[string]http.Response)
+}
 
 var ms MapStore
+var rt RequesterTool
 
 func main() {
-	ms.fetchTasks = make(map[string]FetchTask)
+	ms.init()
+	rt.init()
 	r := echo.New()
-	guid1 := xid.New()
-	ms.fetchTasks[guid1.String()] = FetchTask{ID: guid1.String(), Request: "test1", Method: "GET", Body: "BODY"}
-	guid2 := xid.New()
-	ms.fetchTasks[guid2.String()] = FetchTask{ID: guid2.String(), Request: "test2", Method: "GET", Body: "BODY"}
 	fmt.Println("Server is listening...")
 	r.GET("/task", getTasks)
 	r.GET("/task/:id", getTask)
 	r.POST("/task", addFetchTask)
 	r.DELETE("/task/:id", deleteFetchTask)
+	r.GET("/send/:id", sendTask)
 	log.Fatal(http.ListenAndServe(":8000", r))
 
+}
+
+func sendTask(c echo.Context) error {
+	var taskId = c.Param("id")
+	task, err := ms.getById(taskId)
+	if err == nil {
+		var resp, err = rt.send(task)
+		if err == nil {
+			rt.saveResponse(task, resp)
+		}
+	}
+	return c.String(http.StatusBadRequest, err.Error())
 }
 
 func addFetchTask(c echo.Context) error {
@@ -49,10 +68,8 @@ func addFetchTask(c echo.Context) error {
 	if err = c.Bind(&ft); err != nil {
 		return c.String(http.StatusBadRequest, "Wrong JSON")
 	}
-	guid := xid.New()
-	ft.ID = guid.String()
 	ms.save(&ft)
-	return c.String(http.StatusOK, "Task added \n ID:"+guid.String())
+	return c.String(http.StatusOK, "Task added \n ID:"+ft.ID)
 }
 
 func getTasks(c echo.Context) error {
@@ -82,37 +99,44 @@ func deleteFetchTask(c echo.Context) error {
 	return c.String(http.StatusNotFound, "Task not found")
 }
 
-func (rt RequesterTool) send(task *FetchTask) string {
+func (rt *RequesterTool) send(task *FetchTask) (*http.Response, error) {
 	client := &http.Client{}
 	jsonValue, _ := json.Marshal(task.Body)
 	req, err := http.NewRequest(task.Method, task.Request, bytes.NewBuffer(jsonValue))
 	if err != nil {
-		return err.Error()
+		return &http.Response{}, err
 	}
 	resp, err := client.Do(req)
 	if err != nil {
-		return err.Error()
+		return &http.Response{}, err
 	}
-	defer resp.Body.Close()
-	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return err.Error()
+		return &http.Response{}, err
 	}
-	return string(respBody[:])
+	return resp, nil
 }
 
-func (m MapStore) getById(id string) (FetchTask, error) {
+func (m *MapStore) getById(id string) (*FetchTask, error) {
 	_, ft := ms.fetchTasks[id]
 	if ft {
-		return ms.fetchTasks[id], nil
+		var task = ms.fetchTasks[id]
+		return &task, nil
 	}
-	return FetchTask{}, errors.New("Not found")
+	return &FetchTask{}, errors.New("Not found")
 }
 
-func (m MapStore) save(ft *FetchTask) {
+func (m *MapStore) save(ft *FetchTask) {
+	m.generateID(ft)
 	ms.fetchTasks[ft.ID] = *ft
 }
 
-func (m MapStore) delete(id string) {
+func (m *MapStore) delete(id string) {
 	delete(ms.fetchTasks, id)
+}
+
+func (m *MapStore) generateID(ft *FetchTask) {
+	ft.ID = xid.New().String()
+}
+func (rt *RequesterTool) saveResponse(ft *FetchTask, resp *http.Response) {
+	rt.requests[ft.ID] = *resp
 }
